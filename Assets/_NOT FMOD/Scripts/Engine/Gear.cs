c0cf9761c3acbe21b0dad1ceab1d821b8ea560e7
Phil Harmonic <cornucanis@yahoo.com>
2018-11-03 05:45:27 -0500

    set up initial directories and simple gear demo

---
+++ Assets/_NOT FMOD/Scripts/Engine/Gear.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gear : MonoBehaviour {
+
+	//config params
+	[SerializeField] Gear[] childGears;
+	public int teeth;
+
+
+	public enum Direction {Clockwise, Counterclockwise}
+
+	public void Rotate(float rotationAmount, Direction dir) {
+		bool isClockwise = dir == Direction.Clockwise;
+		int dirMulti = isClockwise ? 1 : -1;
+		transform.Rotate (0, 0, rotationAmount * dirMulti);
+		for (int i = 0; i < childGears.Length; i++) {
+			float toothRatio = 1f * teeth / childGears [i].teeth;
+			childGears [i].Rotate (rotationAmount * toothRatio, isClockwise ? Direction.Counterclockwise : Direction.Clockwise);
+			if (childGears [i].name == "Big Gear") {
+				Debug.Log ("Ratio: " + toothRatio + ", amount: " + rotationAmount + ", total: " + rotationAmount * toothRatio + ", teeth: " + teeth + " " + childGears [i].teeth);
+			}
+		}
+	}
+}
---
+++ Assets/_NOT FMOD/Scripts/Engine/GearKeyboardController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GearKeyboardController : MonoBehaviour {
+
+	[SerializeField] Gear driver;
+	[SerializeField] float rotationSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float horizontalInput = Input.GetAxisRaw ("Horizontal");
+		if (horizontalInput != 0f) {
+			Gear.Direction dir = horizontalInput > 0 ? Gear.Direction.Clockwise : Gear.Direction.Counterclockwise;
+			driver.Rotate (rotationSpeed, dir);
+		}
+	}
+
+	public void ChangeSpeed (float newSpeed) {
+		rotationSpeed = newSpeed;
+	}
+}
