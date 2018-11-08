6c6e53f0a92591b54eec302bad9ebb3478afb1dd
Phil Harmonic <cornucanis@yahoo.com>
2018-11-08 08:12:28 -0600

    more animation work, movement tweaks, character swapping

--- Assets/_NOT FMOD/Scripts/Player/PlayerMovement.cs
+++ Assets/_NOT FMOD/Scripts/Player/PlayerMovement.cs
@@ -4,17 +4,83 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
+	[SerializeField] float runSpeed = 2f;
+
+	//cached references
+	PlayerStatus playerStatus;
+	Rigidbody2D rb;
+
+	//state variables
+	float horizontalInput;
+	float verticalInput;
+	public PlayerStatus.State currentState;
+	bool facingRight = true;
+
 	// Use this for initialization
 	void Start () {
-		
+		playerStatus = GetComponent<PlayerStatus> ();
+		rb = GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		CheckInput ();
+		HandleInput ();
+	}
+
+	void CheckInput() {
+		horizontalInput = Input.GetAxis ("Horizontal");
+		verticalInput = Input.GetAxis ("Vertical");
+		if (Input.GetButtonDown ("Jump")) {
+			Debug.Log(playerStatus.Jump ());
+		}
+	}
+
+	void HandleInput() {
+		if (Mathf.Abs (horizontalInput) > Mathf.Epsilon && playerStatus.CanMove()) {
+			Move ();
+		}
+	}
+
+	void Move() {
+		if (currentState != PlayerStatus.State.Walk) {
+			playerStatus.CurrentState = PlayerStatus.State.Walk;
+		}
+		FlipSprite ();
+		rb.velocity = new Vector2( horizontalInput * runSpeed, 0f);
+	}
+
+	void FlipSprite() {
+		if (facingRight) {
+			if (horizontalInput < 0f) {
+				transform.localScale = new Vector3 (-1, 1, 1);
+				facingRight = false;
+			}
+		} else {
+			if (horizontalInput > 0f) {
+				transform.localScale = new Vector3 (1, 1, 1);
+				facingRight = true;
+			}
+		}
+	}
+
+	void StopMovement() {
+		rb.velocity = Vector2.zero;
 	}
 
 	public void OnWalkStay() {
 
+		if (Mathf.Abs (horizontalInput) < Mathf.Epsilon) {
+			rb.velocity = Vector2.zero;
+			playerStatus.CurrentState = PlayerStatus.State.Idle;
+		}
+	}
+
+	public void TranslateX (float amt) {
+		transform.position += new Vector3 (amt, 0f, 0f);
+	}
+
+	public void TranslateY(float amt) {
+		transform.position += new Vector3 (0f, amt, 0f);
 	}
 }
--- Assets/_NOT FMOD/Scripts/Player/PlayerStatus.cs
+++ Assets/_NOT FMOD/Scripts/Player/PlayerStatus.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerStatus : MonoBehaviour {
 
 	//config params
+
+	[SerializeField] float jumpForce = 200f;
+
 	[Header("Sprite Configuration")]
 	[SerializeField] SpriteRenderer characterSprite;
 	[SerializeField] SpriteRenderer weaponSprite;
@@ -17,11 +20,13 @@ public class PlayerStatus : MonoBehaviour {
 	PlayerCombat playerCombat;
 	Rigidbody2D rb;
 	CapsuleCollider2D coll;
+	LayerMask jumpMask;
 
 	//state variables
 	State currentState;
 	int jumpMode;
-	bool isSword;
+	bool isSword = true;
+	bool armed = true;
 
 
 	public enum State {Idle, Walk, Jump, Attack, Stunned, Death}
@@ -37,7 +42,7 @@ public class PlayerStatus : MonoBehaviour {
 	}
 
 	void Awake() {
-		anim = GetComponent<Animator> ();
+		anim = GetComponentInParent<Animator> ();
 		playerMovement = GetComponent<PlayerMovement> ();
 		playerCombat = GetComponent<PlayerCombat> ();
 		rb = GetComponent<Rigidbody2D> ();
@@ -47,12 +52,51 @@ public class PlayerStatus : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		jumpMask = LayerMask.GetMask ("Foreground");
+		//Debug.Log (coll.bounds + " " + coll.bounds.extents);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetButtonDown ("Switch")) {
+			CharacterSwap ();
+		}
+		Invoke (currentState.ToString () + "Stay", 0f);
+		TrackWeapon ();
+	}
+
+	void TrackWeapon() {
+		if (armed) {
+			weaponSprite.transform.position = transform.position;
+		}
+	}
+
+	public bool CanMove() {
+		if (currentState != State.Death && currentState != State.Stunned && currentState != State.Jump) {
+			return true;
+		}
+		return false;
+	}
+
+	public bool Jump() {
+
+		if (CheckGrounded()) {
+			rb.AddForce (new Vector2 (0f, jumpForce));
+			return true;
+		}
+		return false;
+	}
+
+	private bool CheckGrounded() {
+		//Debug.Log ("center: " + (coll.bounds.center - Vector3.down * 0.1f) + ", truecenter: " + coll.bounds.center + ", size: " + new Vector2 (coll.size.x - 0.1f, coll.size.y));
+		Collider2D colliderCheck = Physics2D.OverlapCapsule (coll.bounds.center - Vector3.down * 0.5f, new Vector2 (coll.size.x - 0.1f, coll.size.y), CapsuleDirection2D.Vertical, 0f, jumpMask);
+		if (colliderCheck) {
+			Debug.Log (colliderCheck.name);
+			return true;
+		} else {
+			return false;
+		}
+		//return Physics.CheckCapsule (coll.bounds.center, new Vector3 (coll.bounds.center.x, coll.bounds.min.y, coll.bounds.center.z), coll.bounds.extents.x - 1f, jumpMask);
 	}
 
 	public void ChangeStates(State newState) {
@@ -63,15 +107,17 @@ public class PlayerStatus : MonoBehaviour {
 		Invoke (newStateName + "Enter", 0f);
 		anim.SetBool (currentStateName, false);
 		anim.SetBool (newStateName, true);
+		playerMovement.currentState = currentState;
 	}
 
 	private void CharacterSwap() {
 		isSword = !isSword;
+		anim.SetBool ("isSword", isSword);
 		Vector3 charPos = characterSprite.transform.position;
 		characterSprite.transform.position = weaponSprite.transform.position;
 		weaponSprite.transform.position = charPos;
 		if (isSword) {
-
+			
 		} else {
 
 		}
