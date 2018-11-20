b35945d5ef6f9bd64605b597d2ceea4f442a5a4a
Unknown <alessandro.fama@ymail.com>
2018-11-20 01:37:09 +0100

    FMOD integration, Animation Event creation, Script Setup

---
+++ Assets/_NOT FMOD/Scripts/Audio/FMODPaths.cs
@@ -0,0 +1,25 @@
+﻿public static class FMODPaths
+{
+    // EVENTS
+   
+    //character
+    public static readonly string FOOTSTEPS_STONE = "event:/char/footsteps_stone";
+    public static readonly string ATTACK_1 = "event:/char/attack_1";
+    public static readonly string ATTACK_2 = "event:/char/attack_2";
+    public static readonly string ATTACK_3 = "event:/char/attack_3";
+    public static readonly string FALL_SNOW = "event:/char/fall_snow";
+    public static readonly string FALL_SNOW_LIGHT = "event:/char/fall_snow_light";
+    public static readonly string FALL_STONE = "event:/char/fall_stone";
+    public static readonly string FALL_STONE_LIGHT = "event:/char/fall_stone_light";
+    public static readonly string JUMP = "event:/char/jump";
+
+
+
+    // PARAMETERS
+    public static readonly string FALL_IMPACT = "Impact";
+
+
+    // SNAPSHOTS
+
+
+}
